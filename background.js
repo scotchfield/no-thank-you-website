fe72694bf823e94f5d5500a88bf48a0a35d3e546
scotchfield <swrittenb@gmail.com>
2016-12-08 16:40:58 -0500

    Redirect to about:blank if a tab URL matches a substring

--- background.js
+++ background.js
@@ -1,4 +1,18 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  //chrome.tabs.update(tab.id, {url: 'about:blank'});
-  
+  if (! tab.url) {
+    return;
+  }
+
+  chrome.storage.sync.get({
+    blockedSites: '',
+  }, function(storage) {
+    var blockedSites = storage.blockedSites.split('\n'),
+      shouldBlock = false;
+
+    blockedSites.forEach(function (url) {
+      if (tab.url.indexOf(url) >= 0) {
+        chrome.tabs.update(tab.id, {url: 'about:blank'});
+      }
+    });
+  });
 });
